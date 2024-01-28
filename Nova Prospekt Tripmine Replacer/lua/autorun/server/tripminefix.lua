7424080de8eb83730f99f5c21f09445cab22483c
Lassi Jokinen <lassijokinen00@gmail.com>
2024-01-28 14:58:25 +0200

    Added a map I missed

--- Nova Prospekt Tripmine Replacer/lua/autorun/server/tripminefix.lua
+++ Nova Prospekt Tripmine Replacer/lua/autorun/server/tripminefix.lua
@@ -5,6 +5,8 @@ SLAM2 = ents.Create("npc_tripmine")
 SLAM3 = ents.Create("npc_tripmine")
 SLAM4 = ents.Create("npc_tripmine")
 SLAM5 = ents.Create("npc_tripmine")
+SLAM6 = ents.Create("npc_tripmine")
+SLAM7 = ents.Create("npc_tripmine")
 
 if set == 1 then
 
@@ -57,6 +59,40 @@ if IsValid(ent) then
 ent:TakeDamage(ent:Health())
 
 timer.Simple(0.5, function()
+SLAM1:SetPos(Vector(-2105.905273, -572.968750, 531.286255))
+SLAM1:SetAngles(Angle(90, 90, 0))
+SLAM1:Spawn()
+SLAM2:SetPos(Vector(-1382.281372, -979.011230, 525.929260))
+SLAM2:SetAngles(Angle(90, 270, 0))
+SLAM2:Spawn()
+SLAM3:SetPos(Vector(-957.391663, -979.011230, 524.795532))
+SLAM3:SetAngles(Angle(90, 270, 0))
+SLAM3:Spawn()
+SLAM4:SetPos(Vector(-950.933655, -1212.968750, 587.963745))
+SLAM4:SetAngles(Angle(90, 90, 0))
+SLAM4:Spawn()
+SLAM5:SetPos(Vector(-844.968750, -951.221436, 522.969299))
+SLAM5:SetAngles(Angle(90, 0, 0))
+SLAM5:Spawn()
+SLAM6:SetPos(Vector(-892.968750, -84.720947, 523.833496))
+SLAM6:SetAngles(Angle(90, 0, 0))
+SLAM6:Spawn()
+SLAM7:SetPos(Vector(-323.031250, 2079.142822, 410.951508))
+SLAM7:SetAngles(Angle(90, 180, 0))
+SLAM7:Spawn()
+end)
+end
+end)
+
+end
+
+if set == 4 then
+
+timer.Simple(1.1, function()
+if IsValid(ent) then
+ent:TakeDamage(ent:Health())
+
+timer.Simple(0.5, function()
 SLAM1:SetPos(Vector(1180.270996, -1871.316895, -236.968750))
 
 SLAM1:SetAngles(Angle(360, 0, 0))
@@ -69,19 +105,19 @@ end
 
 end
 
-local TableOfMines = {"slam_1", "slam_1_trigger", "slam_2", "slam_2_trigger", "slam_3", "slam_3_trigger", "slam_4", "slam_4_trigger", "slam_5", "slam_5_trigger"}
-
 hook.Add("PostCleanupMap", "NovaCleanup", function()
 
 for _,Mine in pairs(ents.FindByClass("func_breakable")) do
-if IsValid(Mine) and table.HasValue(TableOfMines, Mine:GetName()) then
+if IsValid(Mine) and string.find(Mine:GetName(), "slam_") then
 
 if game.GetMap() == "d2_prison_03" then
 ReplaceSet(1, Mine)
 elseif game.GetMap() == "d2_prison_04" then
 ReplaceSet(2, Mine)
-elseif game.GetMap() == "d2_prison_06" then
+elseif game.GetMap() == "d2_prison_05" then
 ReplaceSet(3, Mine)
+elseif game.GetMap() == "d2_prison_06" then
+ReplaceSet(4, Mine)
 end
 
 end
@@ -92,14 +128,16 @@ end)
 hook.Add("InitPostEntity", "ReplaceNovaProspektFakeMines", function()
 
 for _,Mine in pairs(ents.FindByClass("func_breakable")) do
-if IsValid(Mine) then
+if IsValid(Mine) and string.find(Mine:GetName(), "slam_") then
 
 if game.GetMap() == "d2_prison_03" then
 ReplaceSet(1, Mine)
 elseif game.GetMap() == "d2_prison_04" then
 ReplaceSet(2, Mine)
-elseif game.GetMap() == "d2_prison_06" then
+elseif game.GetMap() == "d2_prison_05" then
 ReplaceSet(3, Mine)
+elseif game.GetMap() == "d2_prison_06" then
+ReplaceSet(4, Mine)
 end
 
 end
